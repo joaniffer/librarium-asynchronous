18119298ffbdc32c5fd81d8146dd627ab7b539fe
joaniffer <prcsn.lvr@gmail.com>
2025-08-22 23:56:30 -0600

    Fix sorting on past books

--- past-books.js
+++ past-books.js
@@ -1,6 +1,6 @@
 BOOKS
   .filter(findBookByDateCondition((_, end) => now > end))
-  .sort((a, b) => a < b ? -1 : 1)
+  .sort((a, b) => a.startDate < b.startDate ? 1 : -1)
   .forEach(book => {
     const pane = document.createElement('div');
     pane.className = 'pane';
